1f3f93878c17b41a48d93e739e6da332166fbb86
Justin Bledsoe <nonce1one@gmail.com>
2022-12-21 20:45:21 -0500

    added .env

--- routes/api/bundlr.js
+++ routes/api/bundlr.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Bundlr = require('@bundlr-network/client');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 let serverBundlr;
 
 async function serverInit() {
-  const key = '...'; // your private key
+  const key = process.env.BUNDLR_PROVIDER; // your private key
   serverBundlr = new Bundlr('https://devnet.bundlr.network', 'ethereum', key);
   const presignedHash = serverBundlr.currencyConfig.sign(
     'sign this message to connect to Bundlr.Network'
