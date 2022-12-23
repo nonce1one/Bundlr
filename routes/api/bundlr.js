faeb6505ba51ff169a1ebb062defed2e92375067
Justin Bledsoe <nonce1one@gmail.com>
2022-12-23 18:53:23 -0500

    udated backend

--- routes/api/bundlr.js
+++ routes/api/bundlr.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const ethers = require('ethers');
 require('dotenv').config();
 
+let provider;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'developement') {
+  console.log('Development Build');
+  provider = 'Development';
+} else {
+  console.log('Production Build');
+  provider = 'Production';
+}
+
 router.get('/', async (req, res) => {
   try {
-    res.json({ message: 'get response from bundlr server!' });
+    res.json({ message: `get response from bundlr ${provider} server!` });
   } catch (err) {
     console.error(err);
     res.status(500).send('server error');
